a4d675391830975f51fa6d2bd95148c211faa79a
LucianoMartinez <Lucianom764@gmail.com>
2023-10-29 08:02:29 -0300

    mod cart button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,43 +8,46 @@ function NavBar() {
 		setTriger(!triger)
 	}
 	return (
-		<>
-			<nav className="bg-gray-900 fixed w-100 z-20 top-0 left-0 right-0 border-b border-gray-600">
-				<div className="max-w flex flex-wrap items-center justify-between mx-auto p-4">
-					<a href="https://flowbite.com/" class="flex items-center">
-						<span className="self-center text-2xl font-semibold whitespace-nowrap text-white">El Logo</span>
-					</a>
-					<div className="flex md:order-2">
+		<nav className="bg-gray-900 fixed w-100 z-20 top-0 left-0 right-0 border-b border-gray-600">
+			<div className="max-w flex flex-wrap items-center justify-between mx-auto p-4">
+				<a href="https://flowbite.com/" class="flex items-center">
+					<span className="self-center text-2xl font-semibold whitespace-nowrap text-white">El Logo</span>
+				</a>
+				<div className="flex md:order-2 gap-3">
 
-						<button type="button" className="text-white focus:ring-4 focus:outline-nonefont-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
-							<BsMinecart />
-							<p>8</p>
-						</button>
-						<button className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" onClick={toggle}>
-							<RxHamburgerMenu />
-						</button>
-					</div>
-					{/* enlaces */}
-					<div className={`items-center justify-between ${triger ? `` : `hidden`} w-full md:flex md:w-auto md:order-1`}>
-						<ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-							<li>
-								<a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
-							</li>
-							<li>
-								<a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Nosotros</a>
-							</li>
-							<li>
-								<a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Servicios</a>
-							</li>
-							<li>
-								<a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contacto</a>
-							</li>
-						</ul>
-					</div>
+					<button type="button" class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+						<BsMinecart />
+
+						<span class="inline-flex items-center justify-center w-4 h-4 ml-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
+							3
+						</span>
+					</button>
+
+
+
+					<button className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" onClick={toggle}>
+						<RxHamburgerMenu />
+					</button>
 				</div>
-			</nav>
-		</>
-
+				{/* enlaces */}
+				<div className={`items-center justify-between ${triger ? `` : `hidden`} w-full md:flex md:w-auto md:order-1`}>
+					<ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+						<li>
+							<a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+						</li>
+						<li>
+							<a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Nosotros</a>
+						</li>
+						<li>
+							<a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Servicios</a>
+						</li>
+						<li>
+							<a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contacto</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
 	)
 }
 

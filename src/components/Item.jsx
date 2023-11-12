e4dda583765e6df2aec23f3c350fdd0d7700e36a
LucianoMartinez <Lucianom764@gmail.com>
2023-11-12 18:06:57 -0300

    Agregando item count del item para agregado de producto

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,17 +1,18 @@
-const Item = ({titulo,imagen}) => {
+import ItemCount from "./ItemCount"
+
+const Item = ({titulo,imagen,precio}) => {
 	
 	return (
-
-
-		<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-			<h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{titulo}</h5>
-			<img class="p-8 rounded-t-lg" src={imagen} alt="product image" />
-			<div class="px-5 pb-5">
-				<div class="flex items-center justify-between">
-					<span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-					<a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+		<div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+			<h5 className="text-xl ml-4 font-semibold tracking-tight text-gray-900 dark:text-white">{titulo}</h5>
+			<img className="p-8 rounded" src={imagen} alt="product image" />
+			<ItemCount />
+			{/* <div className="px-5 pb-5">
+				<div className="flex items-center justify-between">
+					<span className="text-3xl font-bold text-gray-900 dark:text-white">${precio}</span>
+					<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Agregar</button>
 				</div>
-			</div>
+			</div> */}
 		</div>
 
 	)
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,17 +3,17 @@ import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
 import Item from "./Item";
 
 
+
 const ItemList = ({ productos }) => {
 
-	/* al recibir el array podria empezar a diferenciar que mostrar */
 	return (
 		<>
-
 			{productos.map(e =>
 				<Item
 					key={e.id}
 					titulo={e.title}
 					imagen={e.image}
+					precio={e.price}
 				/>)}
 		</>
 	)
